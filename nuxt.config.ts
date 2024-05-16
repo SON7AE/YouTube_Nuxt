2e7cffb3dc62648398bf2936166d2e66462a95da
Marco <bobsot0225@gmail.com>
2024-05-16 10:23:00 +0900

    News API를 활용한 YouTube Vue.js 콘텐츠 개발 中

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@pinia/nuxt'],
+    modules: ["@pinia/nuxt"],
     vite: {
         css: {
             preprocessorOptions: {
@@ -12,4 +12,4 @@ export default defineNuxtConfig({
             },
         },
     },
-})
+});
